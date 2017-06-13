cba6e6d142c858286c23736845d84b2dba2ae04b
Robert Wagner <rwwagner90@gmail.com>
2017-06-12 22:29:20 -0400

    Start setting up meta

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,14 @@
 import Route from 'ember-route';
+import service from 'ember-service/inject';
 
 export default Route.extend({
+  headData: service(),
+  afterModel() {
+    return this.get('headData').setProperties({
+      title: 'Cancer Awareness - Ember.js',
+      description: 'Join us, and the rest of the Ember community in the fight against cancer.',
+      type: 'website',
+      url: ''
+    });
+  },
 });
